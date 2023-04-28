0995bc3352e38137f064b8d21cec97d171c2161d
Aaditya Jagdale <81344234+Contro1-cs@users.noreply.github.com>
2023-04-28 09:54:03 +0530

    final

--- page.py
+++ page.py
@@ -1,6 +1,7 @@
 import openai
 import streamlit as st
 
+openai.api_key = "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"
 
 
 def app():
@@ -10,14 +11,23 @@ def app():
     user_input = st.text_input("You can be as specific as you want!")
     name = 'Niranjan Mathur'
     wordCount = '300'
-    tone = 'formal'
-    editedPrompt = user_input + 'use my full name as ' + name + \
-        'keeping the word count up to ' + wordCount + 'and keep the tone ' + tone
+    tone = 'casual'
+    notes = ''
+    editedPrompt = 'Write a detailed email on the following topic: ' + user_input + '. use my full name as ' + name + \
+        'keeping the word count up to ' + wordCount + 'and keep the tone ' + \
+        tone + '. Additionally also mention' + notes
 
     if st.button("Submit"):
         try:
+            # if (wordCount <= 300):
+            #     maxTokens = 521
+            # elif (wordCount < 600):
+            #     maxTokens = 800
+            # else:
+            #     maxTokens = 1024
+            maxTokens = 1024
             response = openai.Completion.create(
-                engine="text-davinci-002", prompt=editedPrompt, max_tokens=1024
+                engine="text-davinci-002", prompt=editedPrompt, max_tokens=maxTokens
             )
             st.write(response["choices"][0]["text"])
         except Exception as e:
